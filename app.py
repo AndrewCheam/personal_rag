2d9fe97aefb30edf97d44af90696555a93832cc2
Andrew Cheam <cheamandrew@gmail.com>
2024-12-26 18:54:04 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import time
+from query_data import query_rag
+from query_data_with_hist import query_rag_with_history
+
+
+# Streamed response emulator
+def response_generator(response: str):
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
+
+st.title("Simple chat")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        llm_answer = query_rag_with_history(prompt)
+        response = st.write_stream(response_generator(llm_answer))
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
---
+++ query_data_with_hist.py
@@ -0,0 +1,109 @@
+from typing import List
+
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.messages import BaseMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from pydantic import BaseModel, Field
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+
+import argparse
+from langchain_community.vectorstores import Chroma
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms.ollama import Ollama
+
+from get_embedding_function import get_embedding_function
+
+from langsmith import wrappers, traceable
+import os
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+System: You are an expert board game professor that simply and clearly explains board game details. 
+You have a geeky way of talking and always start the conversation by saying something wise!
+
+Based on the history of this conversation:
+
+{history}
+
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = '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'
+
+class InMemoryHistory(BaseChatMessageHistory, BaseModel):
+    """In memory implementation of chat message history."""
+
+    messages: List[BaseMessage] = Field(default_factory=list)
+
+    def add_messages(self, messages: List[BaseMessage]) -> None:
+        """Add a list of messages to the store"""
+        self.messages.extend(messages)
+
+    def clear(self) -> None:
+        self.messages = []
+
+# Here we use a global variable to store the chat message history.
+# This will make it easier to inspect it to see the underlying results.
+store = {}
+
+def get_by_session_id(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = InMemoryHistory()
+    return store[session_id]
+
+def main():
+    # Create CLI.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+    query_rag_with_history(query_text)
+
+
+def query_rag_with_history(query_text: str):
+    # Prepare the DB.
+    embedding_function = get_embedding_function()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_score(query_text, k=5)
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    # prompt = prompt_template.format(context=context_text, question=query_text)
+    
+    model = Ollama(model="llama3.2:latest")
+    chain = prompt_template | model
+
+    chain_with_history = RunnableWithMessageHistory(
+        chain,
+        # Uses the get_by_session_id function defined in the example
+        # above.
+        get_by_session_id,
+        input_messages_key="question",
+        context_messages_key="context",
+        history_messages_key="history",
+    )
+    response_text = chain_with_history.invoke(  # noqa: T201
+        {"question": query_text, "context": context_text},
+        config={"configurable": {"session_id": "foo"}}
+    )
+
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
+
+    print(store)
+    return response_text
+
+if __name__ == "__main__":
+    main()
+
